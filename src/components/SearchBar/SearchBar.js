872ae3922eaa504cbf0de54b3e062ed854f8ae45
seolacarrano <seola6@gmail.com>
2020-11-19 16:43:11 -0500

    finished the main page skeleton

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -18,4 +18,26 @@ class SearchBar extends React.Component {
         }
         );
     }
+
+    render() {
+        return (
+        <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}      
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+      )
+
+    }
 }
+
+export default SearchBar
