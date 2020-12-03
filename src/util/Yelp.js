835698895c06d2543c4ed6474732b4a7f9aedaae
seolacarrano <seola6@gmail.com>
2020-12-03 18:38:57 -0500

    added CORS

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,7 @@
 const apiKey = e58ZvsEUeySYsB2numoJ4yCOYrwf98STXE-O8j3K1NepDu6Pz14Aw6HJPKiIub-lcmGCxjlzOD1U88cY4eWEiI2-aXpnyg8E6EPGy5Zj3Jobxvo1lMuT98N2QnXJX3Yx
 
-const yelp = {}
+const yelp = {};
 
-search (term, location, sortBy) {
-    return fetch (https://api.yelp.com/v3/businesses/search?term=TERM&location=LOCATION&sort_by=SORT_BY)
+search(term, location, sortBy) {
+    return fetch (`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term={TERM}&location={LOCATION}&sort_by={SORT_BY}`);
 }
