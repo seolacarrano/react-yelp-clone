a18fd66520e5ffda09172bde6b1deb97ca160581
seolacarrano <seola6@gmail.com>
2020-12-03 20:32:11 -0500

    yelp api

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,5 +3,34 @@ const apiKey = e58ZvsEUeySYsB2numoJ4yCOYrwf98STXE-O8j3K1NepDu6Pz14Aw6HJPKiIub-lc
 const yelp = {};
 
 search(term, location, sortBy) {
-    return fetch (`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term={TERM}&location={LOCATION}&sort_by={SORT_BY}`);
+    return fetch (`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term={TERM}&location={LOCATION}&sort_by={SORT_BY}`, 
+    { 
+        headers: {
+          Authorization: `Bearer ${apiKey}` 
+        }
+})
+    .then(response => {
+    return response.json();
+    })
+    .then(jsonResponse => {
+        if (jsonResponse.business) {
+            return jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageScr: business.image_url,
+                    name: business.name,
+                    address: business.location.address,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                };        
+            });
+        } 
+    })
 }
+
+
+export default Yelp;
