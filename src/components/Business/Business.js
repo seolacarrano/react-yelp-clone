f0f6cb1510cd39e3f9687196897a10c441a6047a
seolacarrano <seola6@gmail.com>
2020-12-08 12:27:28 -0500

    read me

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -8,7 +8,7 @@ class Business extends React.Component {
       return (
     <div className="Business">
         <div className="image-container">
-           <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+           <img src='https://res.cloudinary.com/dqduwnrb1/image/upload/v1607447075/jakub-kapusnak-4f4YZfDMLeU-unsplash_mhmjzm.jpg' alt=''/>
         </div>
         <h2>{this.props.business.name}</h2>
         <div className="Business-information">
