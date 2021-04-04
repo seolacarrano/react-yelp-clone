7eae06a786895dad84a0d737ee20c69cdc6e9bf2
seolacarrano <seola6@gmail.com>
2021-04-04 16:42:27 -0400

    updating yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,7 +4,10 @@ const Yelp = {
   search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
-        Authorization: `Bearer ${apiKey}`
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": `Bearer ${apiKey}`
       }
     }).then(response => {
       return response.json();
